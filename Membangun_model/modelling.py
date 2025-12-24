80d56d02fb44b2fea6c22472e76259b1af8f7a2a
AlvinMI <alvinmahesairawadi@gmail.com>
2025-12-24 19:20:45 +0700

    fix: implementasi mlflow run dan docker build sesuai revisi reviewer

--- Membangun_model/modelling.py
+++ Membangun_model/modelling.py
@@ -3,25 +3,33 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+import os
 
 mlflow.autolog()
 
 print("Memuat data...")
-df = pd.read_csv('preprocessing/heart_disease_preprocessing/heart_clean.csv')
+path_data = 'namadataset_preprocessing/heart_preprocessed.csv'
+
+if not os.path.exists(path_data):
+    path_data = 'heart_preprocessed.csv'
+
+df = pd.read_csv(path_data)
 
 X = df.drop(columns=['HeartDisease'])
 y = df['HeartDisease']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-with mlflow.start_run(run_name="Basic_Autolog_Model"):
-    print("Training model (Basic)...")
-    
+print("Mulai Training...")
+with mlflow.start_run(run_name="Heart_Disease_Final_Model") as run:
     model = RandomForestClassifier(n_estimators=100, random_state=42)
     model.fit(X_train, y_train)
     
     preds = model.predict(X_test)
     acc = accuracy_score(y_test, preds)
-    print(f"Selesai! Akurasi: {acc}")
+    print(f"Akurasi: {acc}")
 
-print("✅ modelling.py selesai dijalankan.")
+    with open("last_run_id.txt", "w") as f:
+        f.write(run.info.run_id)
+
+print("✅ Model selesai dilatih dan Run ID telah disimpan.")
--- Workflow-CI/MLProjectFolder/modelling.py
+++ Workflow-CI/MLProjectFolder/modelling.py
@@ -3,11 +3,17 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+import os
 
 mlflow.autolog()
 
 print("Memuat data...")
-df = pd.read_csv('heart_preprocessing.csv')
+path_data = 'namadataset_preprocessing/heart_preprocessed.csv'
+
+if not os.path.exists(path_data):
+    path_data = 'heart_preprocessed.csv'
+
+df = pd.read_csv(path_data)
 
 X = df.drop(columns=['HeartDisease'])
 y = df['HeartDisease']
@@ -15,14 +21,15 @@ y = df['HeartDisease']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 print("Mulai Training...")
-with mlflow.start_run() as run:
+with mlflow.start_run(run_name="Heart_Disease_Final_Model") as run:
     model = RandomForestClassifier(n_estimators=100, random_state=42)
     model.fit(X_train, y_train)
     
     preds = model.predict(X_test)
-    print(f"Akurasi: {accuracy_score(y_test, preds)}")
+    acc = accuracy_score(y_test, preds)
+    print(f"Akurasi: {acc}")
 
     with open("last_run_id.txt", "w") as f:
         f.write(run.info.run_id)
 
-print("✅ Model selesai dilatih.")
+print("✅ Model selesai dilatih dan Run ID telah disimpan.")
